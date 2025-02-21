7b12bd9f401e00e29030d0cd9f2ae310935210fc
felixx78 <petrenkov605@gmail.com>
2025-02-21 17:47:28 +0200

    update app.controler

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,25 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, NotFoundException, Param, Query, ValidationPipe } from '@nestjs/common';
+import { RecipeApiService } from './recipe-api/recipe-api.service';
+import { RecipeApiFilterMeatsDto } from './recipe-api/recipe-api.dto';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly repiceApiService: RecipeApiService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('search')
+  async searchMeals(@Query('s') s: string) {
+    return this.repiceApiService.searchMeals(s);
+  }
+
+  @Get('filter')
+  filterMeals(@Query(new ValidationPipe()) query: RecipeApiFilterMeatsDto) {
+    return this.repiceApiService.filterMeals(query.value, query.filterBy);
+  }
+
+  @Get(':id')
+  async getMealById(@Param('id') id: string) {
+    const meal = await this.repiceApiService.getMeatById(id);
+    if (!meal) throw new NotFoundException();
+    return meal;
   }
 }
