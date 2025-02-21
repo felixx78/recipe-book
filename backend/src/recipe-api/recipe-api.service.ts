b4761edfafad16d532261808615c5917d444f1b8
felixx78 <petrenkov605@gmail.com>
2025-02-21 16:51:15 +0200

    recipe-api.service filter -> filterMealsSearch

--- backend/src/recipe-api/recipe-api.service.ts
+++ backend/src/recipe-api/recipe-api.service.ts
@@ -9,12 +9,12 @@ export class RecipeApiService {
 
   constructor(private readonly httpService: HttpService) {}
 
-  async searchMeats(s: string) {
+  async searchMeals(s: string) {
     const response = await firstValueFrom(this.httpService.get<RecipeApiResponsesSearch>(this.BASE_URL + '/search.php?s=' + s));
     return response.data.meals;
   }
 
-  async filter(value: string, filterKey: RecipeApiResponsesFilterOptions) {
+  async filterMealsSearch(value: string, filterKey: RecipeApiResponsesFilterOptions) {
     const response = await firstValueFrom(
       this.httpService.get<RecipeApiResponsesSearch>(this.BASE_URL + '/filter.php?' + filterKey === 'country' ? 'a' : filterKey[0] + '=' + value),
     );
