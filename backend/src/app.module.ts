78b30c228d40903a3aa781aa41621808e6e13c62
felixx78 <petrenkov605@gmail.com>
2025-02-21 16:43:14 +0200

    delete app.service

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { RecipeApiModule } from './recipe-api/recipe-api.module';
 
 @Module({
   imports: [RecipeApiModule],
   controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
