89bb2e4de5322c9b8940abeb78a4c050a10f2804
felixx78 <petrenkov605@gmail.com>
2025-02-21 17:10:56 +0200

    fix recipe-api.dto type

--- backend/src/recipe-api/recipe-api.dto.ts
+++ backend/src/recipe-api/recipe-api.dto.ts
@@ -3,5 +3,5 @@ import { RecipeApiResponsesFilterOptions } from './recipe-api-responses.type';
 
 export class RecipeApiFilterMeatsDto {
   @Validate((_, value) => Object.keys(RecipeApiResponsesFilterOptions).includes(value))
-  filterBy: string;
+  filterBy: keyof typeof RecipeApiResponsesFilterOptions;
 }
